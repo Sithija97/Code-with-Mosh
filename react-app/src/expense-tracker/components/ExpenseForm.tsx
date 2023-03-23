d1bedd50315658ed6303022eaf788026a0c6f8ed
Sithija Shehara <nsithijashehara@gmail.com>
2023-03-23 06:28:57 +0530

    added forms

---
+++ react-app/src/expense-tracker/components/ExpenseForm.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { categories } from "../../App";
+
+const schema = z.object({
+  description: z.string().min(3).max(50),
+  amount: z.number().min(0.01).max(1000_000),
+  category: z.enum(categories),
+});
+
+type ExpenseFormData = z.infer<typeof schema>;
+
+const ExpenseForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ExpenseFormData>({ resolver: zodResolver(schema) });
+  return (
+    <form>
+      <div className="mb-3">
+        <label htmlFor="description" className="form-label">
+          Description
+        </label>
+        <input id="description" className="form-control" type="text" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="amount" className="form-label">
+          Amount
+        </label>
+        <input id="amount" className="form-control" type="number" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="category" className="form-label">
+          Category
+        </label>
+        <select id="category" className="form-select">
+          <option value="">All categories</option>
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+};
+
+export default ExpenseForm;
